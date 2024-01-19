a292b0525078f78cd50e67d73a2df495dd93ddb4
Jackson kitsao <kitsaojackson22@gmail.com>
2024-01-19 15:18:37 +0300

    feat: add clap create for CLI args parsing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Init project with name
+    #[arg(short, long)]
+    init: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
 fn main() {
-    // println!("I'm tyr your package manager!");
-    let mut name = String::from("hello ");
-    println!("{}", name);
-    name.push_str("there");
-    println!("{}", name)
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("create a project called  {}!", args.init)
+    }
 }

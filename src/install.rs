f12e06d797b1129b51c4fafa7ecdadbe4abd08a0
Jackson kitsao <kitsaojackson22@gmail.com>
2024-02-25 22:09:29 +0300

    docs: improve README docs for both NPM and Github

--- src/install.rs
+++ src/install.rs
@@ -1,13 +1,10 @@
-/*
+// Install all the dependencies listed within
+// package.json in the local node_modules folder.
 
-Install all the dependencies listed within
-package.json in the local node_modules folder.
+// If tyr.lock is present and is enough to satisfy all the dependencies listed in package.json, the exact versions recorded in tyr.lock are installed, and tyr.lock will be unchanged. tyr will not check for newer versions.
 
-If tyr.lock is present and is enough to satisfy all the dependencies listed in package.json, the exact versions recorded in tyr.lock are installed, and tyr.lock will be unchanged. tyr will not check for newer versions.
+// If tyr.lock is absent, or is not enough to satisfy all the dependencies listed in package.json (for example, if you manually add a dependency to package.json), Tyrr should look for the newest versions available that satisfy the constraints in package.json. The results are written to tyrr.lock.
 
-If tyr.lock is absent, or is not enough to satisfy all the dependencies listed in package.json (for example, if you manually add a dependency to package.json), Tyrr should look for the newest versions available that satisfy the constraints in package.json. The results are written to tyrr.lock.
-
-*/
 use std::collections::{HashMap, HashSet};
 // use std::collections::HashMap;
 use crate::resolve_package_from_registry;
